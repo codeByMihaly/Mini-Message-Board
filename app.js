bae2e3da3e6c287ce60457fb418c7a02579d5f04
Szőke Mihály <szoke.mihaly97@gmail.com>
2026-06-05 17:45:21 +0200

    Bugfixing json file.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("node:path");
 const { indexRouter } = require("./routes/indexRouter");
 const messageRouter = require("./routes/messageRouter");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
